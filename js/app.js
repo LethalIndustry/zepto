a9772d9c442f29759feb8eb971a38edff1b957d6
LethalIndustry <josu3rg@hotmail.com>
2017-09-22 17:49:09 -0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -22,6 +22,7 @@ $(document).on('deviceready',function(){
       url: "login.html",
       success: function(result) {
           $("#contenido").html(result).trigger("create");
+          $("#curp").val(localStorage.getItem("CURP"));
           footer();
       }
     });
@@ -62,8 +63,72 @@ $(document).on('deviceready',function(){
           url: "certificado.html", 
           success: function(result) {
                 $("#contenido").html(result).trigger("create");
+                $("#txtCurp").val(localStorage.getItem("CURP"));
           }
       });
+    });
+
+    $("#contenido").on("click", ".cCertificado" ,function(){
+        curp = $("#txtCurp").val();
+        ciclo = $("#cmbCiclo").val();
+        longitud = curp.length;
+        if(curp==""){
+            navigator.notification.alert('Debe llenar el campo CURP',alertDismissed,'Error', 'Ok');
+            return false;
+        }else{
+            if(longitud!=18){
+                navigator.notification.alert('El campo CURP debe contener 18 caracteres',alertDismissed,'Error', 'Ok');
+                return false;
+            }else{
+                $.mobile.loading( 'show', {
+                  text: 'Por favor espere..',
+                  textVisible: true,
+                  theme: 'b'
+                });
+                $.ajax({
+                  url : 'http://cpte.gob.mx/peie/app/data.php',
+                  type : 'GET',
+                  contentType: "application/json; charset=utf-8",
+                  //dataType : 'json',
+                  data: {curp: curp, ciclo: ciclo},
+                  success : function(data, textStatus, jqxhr){ 
+                    console.log(data);
+                    if(data==1){
+                      $.mobile.loading('hide');
+                      window.open('http://cpte.gob.mx/appCalificaciones/vista/pdf/CERTIFICADO.php?CURP='+curp+'&CICLO='+ciclo, '_system', 'location=yes')
+                    }else{
+                      $.mobile.loading('hide');
+                      navigator.notification.alert('Certificado no encontrado',alertDismissed,'Error', 'Ok');
+                    }
+                    /*if(data!=null){
+                      if(data.length > 0){
+                        for (var x = 0; x < data.length; x++) {
+                            fecha = data[x].fecha;
+                            aviso = data[x].aviso;
+                            departamento = data[x].departamento;
+
+                            $(".ui-content").append("<div class='contenedor'><div class='head'>"+fecha+"</div><div class='dentro'>"+aviso+"</div><div class='foo'>"+departamento+"</div>");
+                        }   
+                        $.mobile.loading('hide'); 
+                      }
+                    }*/
+                    /*else{
+                      $.mobile.loading('hide'); 
+                      navigator.notification.alert('No hay avisos nuevos para mostrar',alertDismissed,'Aviso', 'Ok');
+                      $.ajax({
+                          url: "menu.html",
+                          success: function(result) {
+                              $("#contenido").html(result).trigger("create");
+                          }
+                      });
+                    }*/                               
+                  },
+                  error : function (jqxhr, textStatus, errorMessage){ 
+                      console.log(errorMessage);              
+                  }               
+              });                
+            }
+        }
     });
 
     $("#contenido").on("click", ".avisos" ,function(){
